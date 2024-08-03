f67e27c8c987b7590741c2c8005e5842516c20ac
taijusanagi <taijusanagi@gmail.com>
2024-08-03 11:29:35 +0700

    feat: implement subgraph

--- subgraph/src/mapping.ts
+++ subgraph/src/mapping.ts
@@ -1,107 +1,40 @@
 import {
-  ethereum, BigInt
-} from '@graphprotocol/graph-ts'
+  CreateRegistry as CreateRegistryEvent,
+  EthDrive as EthDriveContract,
+} from "../generated/EthDrive/EthDrive";
+import { Directory } from "../generated/schema";
 
-import {
-  EventToken as EventTokenEvent,
-  Transfer   as TransferEvent,
-} from '../generated/Poap/Poap'
+export function handleCreateRegistry(event: CreateRegistryEvent): void {
+  const contract = EthDriveContract.bind(event.address);
 
-import {
-  Token,
-  Account,
-  Event,
-  Transfer,
-} from '../generated/schema'
+  const path = event.params.path;
+  const segments = path.split("/");
+  let parentDirectory: Directory | null = null;
+  for (let i = 0; i < segments.length; i++) {
+    const id = segments.slice(0, i + 1).join("/");
+    let directory = Directory.load(id);
 
-const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
-
-
-function createEventID(event: ethereum.Event): string
-{
-  return event.block.number.toString().concat('-').concat(event.logIndex.toString());
-}
-
-export function handleEventToken(ev: EventTokenEvent): void
-{
-  let event = Event.load(ev.params.eventId.toString());
-  // This handler always run after the transfer handler
-  let token = Token.load(ev.params.tokenId.toString())!;
-  if (event == null) {
-    event               = new Event(ev.params.eventId.toString());
-    event.tokenCount    = BigInt.fromI32(0);
-    event.tokenMints    = BigInt.fromI32(0);
-    event.transferCount = BigInt.fromI32(0);
-    event.created       = ev.block.timestamp
-  }
-
-  event.tokenCount    += BigInt.fromI32(1);
-  event.tokenMints    += BigInt.fromI32(1);
-  event.transferCount += BigInt.fromI32(1);
-  token.event         = event.id;
-  token.mintOrder     = event.tokenMints;
-  event.save();
-  token.save();
-}
-
-export function handleTransfer(ev: TransferEvent): void {
-  let token    = Token.load(ev.params.tokenId.toString());
-  let from     = Account.load(ev.params.from.toHex());
-  let to       = Account.load(ev.params.to.toHex());
-  let transfer = new Transfer(createEventID(ev));
-
-  if (from == null) {
-    from              = new Account(ev.params.from.toHex());
-    // The from account at least has to own one token
-    from.tokensOwned  = BigInt.fromI32(1);
-  }
-  // Don't subtracts from the ZERO_ADDRESS (it's the one that mint the token)
-  // Avoid negative values
-  if(from.id != ZERO_ADDRESS) {
-    from.tokensOwned -= BigInt.fromI32(1);
-  }
-  from.save();
-
-  if (to == null) {
-    to              = new Account(ev.params.to.toHex());
-    to.tokensOwned  = BigInt.fromI32(0);
-  }
-  to.tokensOwned += BigInt.fromI32(1);
-  to.save();
-
-  if (token == null) {
-    token               = new Token(ev.params.tokenId.toString());
-    token.transferCount = BigInt.fromI32(0);
-    token.created       = ev.block.timestamp
-  }
-  token.owner = to.id;
-  token.transferCount += BigInt.fromI32(1);
-  token.save();
-
-
-  if (token.event != null) {
-    let event = Event.load(token.event as string);
-
-    if(event != null) {
-      // Add one transfer
-      event.transferCount += BigInt.fromI32(1);
-  
-      // Burning the token
-      if(to.id == ZERO_ADDRESS) {
-        event.tokenCount    -= BigInt.fromI32(1);
-        // Subtract all the transfers from the burned token
-        event.transferCount -= token.transferCount;
-      }
-      event.save();
+    if (!directory) {
+      directory = new Directory(id);
+      const tokenId = contract.getTokenIdFromPath(path);
+      directory.tokenId = tokenId;
+      directory.tokenBountAccount = contract.getTokenBoundAccountFromTokenId(
+        tokenId
+      );
+      directory.holder = contract.ownerOf(tokenId);
+      directory.subdirectories = [];
     }
+
+    if (parentDirectory) {
+      let subdirs = parentDirectory.subdirectories;
+      if (!subdirs.includes(directory.id)) {
+        subdirs.push(directory.id);
+        parentDirectory.subdirectories = subdirs;
+        parentDirectory.save();
+      }
+    }
+
+    directory.save();
+    parentDirectory = directory;
   }
-
-  
-
-  transfer.token       = token.id;
-  transfer.from        = from.id;
-  transfer.to          = to.id;
-  transfer.transaction = ev.transaction.hash;
-  transfer.timestamp   = ev.block.timestamp;
-  transfer.save();
 }
