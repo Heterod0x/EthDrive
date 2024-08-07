e8c15cb8c596f84d4fa45300483a136dde462b1b
taijusanagi <taijusanagi@gmail.com>
2024-08-07 17:38:15 +0700

    feat: exproler in config

--- contracts/shared/app/config.ts
+++ contracts/shared/app/config.ts
@@ -2,6 +2,8 @@ export const config = {
   "9999999": {
     path: "tenderly-virtual-testnet",
     alchemyChainName: "",
+    exproler:
+      "https://dashboard.tenderly.co/explorer/vnet/56839ed8-ce5c-4785-bfc7-573f11a9f5ed",
     chainlinkCCIPChainSelecter: "",
     isAccountAbstractionEnabled: false,
     isCCIPEnabled: false,
@@ -9,6 +11,7 @@ export const config = {
   "11155111": {
     path: "sepolia",
     alchemyChainName: "eth-sepolia",
+    exproler: "https://eth-sepolia.blockscout.com",
     chainlinkCCIPChainSelecter: "",
     isAccountAbstractionEnabled: true,
     isCCIPEnabled: false,
@@ -16,6 +19,7 @@ export const config = {
   "11155420": {
     path: "optimism-sepolia",
     alchemyChainName: "opt-sepolia",
+    exproler: "https://optimism-sepolia.blockscout.com",
     chainlinkCCIPChainSelecter: "5224473277236331295",
     isAccountAbstractionEnabled: true,
     isCCIPEnabled: true,
@@ -23,6 +27,7 @@ export const config = {
   "84532": {
     path: "base-sepolia",
     alchemyChainName: "base-sepolia",
+    exproler: "https://base-sepolia.blockscout.com",
     chainlinkCCIPChainSelecter: "10344971235874465080",
     isAccountAbstractionEnabled: true,
     isCCIPEnabled: true,
