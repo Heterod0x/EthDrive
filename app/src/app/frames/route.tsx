ff6ff0580517c86962d0dbf979a6056899ce220e
taijusanagi <taijusanagi@gmail.com>
2024-08-08 12:45:58 +0700

    feat: integrate frame

---
+++ app/src/app/frames/route.tsx
@@ -0,0 +1,32 @@
+import { Button, createFrames } from "frames.js/next";
+
+const frames = createFrames({
+  basePath: "/",
+});
+
+const handleRequest = frames(async () => {
+  return {
+    image: "https://super-eth-drive.vercel.app/logo.png",
+    buttons: [
+      <Button
+        action="tx"
+        target={{
+          pathname: `/frames/txdata`,
+          query: { tokenId: "0x1234" },
+        }}
+        post_url={`/frames`}
+      >
+        Create
+      </Button>,
+    ],
+    textInput: "chain/path",
+    imageOptions: {
+      aspectRatio: "1:1",
+      width: 256,
+      height: 256,
+    },
+  };
+});
+
+export const GET = handleRequest;
+export const POST = handleRequest;
--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "@rainbow-me/rainbowkit/styles.css";
+import { fetchMetadata } from "frames.js/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
@@ -7,10 +8,24 @@ import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+const metadata: Metadata = {
   title: "ETHDrive",
   description: "This is ETHDrive website",
 };
+
+export async function generateMetadata() {
+  const baseUrl =
+    process.env.NODE_ENV == "production"
+      ? "https://super-eth-drive.vercel.app"
+      : "http://localhost:3000";
+
+  return {
+    ...metadata,
+    other: {
+      ...(await fetchMetadata(`${baseUrl}/frames`)),
+    },
+  };
+}
 
 export default function Layout({
   children,
